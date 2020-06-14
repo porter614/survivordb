5e215dbec5d566cc8c82d4adb5a282ce048d499d
Victor Porter <victorporter@Victors-MacBook-Pro.local>
2020-06-13 17:58:58 -0700

    added react auth

--- services/client/src/App.jsx
+++ services/client/src/App.jsx
@@ -7,6 +7,7 @@ import UsersList from "./components/UsersList";
 import AddUser from "./components/AddUser";
 import LoginForm from "./components/LoginForm";
 import RegisterForm from "./components/RegisterForm";
+import UserStatus from "./components/UserStatus";
 
 class App extends Component {
   constructor() {
@@ -21,6 +22,10 @@ class App extends Component {
 
     this.addUser = this.addUser.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.handleRegisterFormSubmit = this.handleRegisterFormSubmit.bind(this);
+    this.handleLoginFormSubmit = this.handleLoginFormSubmit.bind(this);
+    this.isAuthenticated = this.isAuthenticated.bind(this);
+    this.logoutUser = this.logoutUser.bind(this);
   }
 
   componentDidMount() {
@@ -36,6 +41,64 @@ class App extends Component {
       .catch(err => {
         console.log(err);
       });
+  }
+
+  handleRegisterFormSubmit(data) {
+    const url = `${process.env.REACT_APP_USERS_SERVICE_URL}/auth/register`;
+    axios
+      .post(url, data)
+      .then(res => {
+        console.log(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  handleLoginFormSubmit(data) {
+    const url = `${process.env.REACT_APP_USERS_SERVICE_URL}/auth/login`;
+    axios
+      .post(url, data)
+      .then(res => {
+        this.setState({ accessToken: res.data.access_token });
+        this.getUsers();
+        window.localStorage.setItem("refreshToken", res.data.refresh_token); // new
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  isAuthenticated() {
+    if (this.state.accessToken || this.validRefresh()) {
+      return true;
+    }
+    return false;
+  }
+
+  validRefresh() {
+    const token = window.localStorage.getItem("refreshToken");
+    if (token) {
+      axios
+        .post(`${process.env.REACT_APP_USERS_SERVICE_URL}/auth/refresh`, {
+          refresh_token: token
+        })
+        .then(res => {
+          this.setState({ accessToken: res.data.access_token });
+          this.getUsers();
+          window.localStorage.setItem("refreshToken", res.data.refresh_token);
+          return true;
+        })
+        .catch(err => {
+          return false;
+        });
+    }
+    return false;
+  }
+
+  logoutUser() {
+    window.localStorage.removeItem("refreshToken");
+    this.setState({ accessToken: null });
   }
 
   addUser(event) {
@@ -65,7 +128,11 @@ class App extends Component {
   render() {
     return (
       <div>
-        <NavBar title={this.state.title} />
+        <NavBar
+          title={this.state.title}
+          logoutUser={this.logoutUser}
+          isAuthenticated={this.isAuthenticated} // new
+        />
         <section className="section">
           <div className="container">
             <div className="columns">
@@ -94,8 +161,36 @@ class App extends Component {
                     )}
                   />
                   <Route exact path="/about" component={About} />
-                  <Route exact path="/register" component={RegisterForm} />
-                  <Route exact path="/login" component={LoginForm} />
+                  <Route
+                    exact
+                    path="/register"
+                    render={() => (
+                      <RegisterForm
+                        handleRegisterFormSubmit={this.handleRegisterFormSubmit}
+                        isAuthenticated={this.isAuthenticated}
+                      />
+                    )}
+                  />
+                  <Route
+                    exact
+                    path="/login"
+                    render={() => (
+                      <LoginForm
+                        handleLoginFormSubmit={this.handleLoginFormSubmit}
+                        isAuthenticated={this.isAuthenticated}
+                      />
+                    )}
+                  />
+                  <Route
+                    exact
+                    path="/status"
+                    render={() => (
+                      <UserStatus
+                        accessToken={this.state.accessToken}
+                        isAuthenticated={this.isAuthenticated}
+                      />
+                    )}
+                  />
                 </Switch>
               </div>
             </div>
